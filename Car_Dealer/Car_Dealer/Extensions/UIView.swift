750910744da8dd955874e36d67eff4fd66e55e83
Karen <karen606>
2024-10-09 19:19:00 +0400

    create menu

--- Car_Dealer/Car_Dealer/Extensions/UIView.swift
+++ Car_Dealer/Car_Dealer/Extensions/UIView.swift
@@ -27,9 +27,27 @@ extension UIView {
     }
     
     func addBottomBorder(color: UIColor, thickness: CGFloat) {
-            let bottomBorder = CALayer()
-            bottomBorder.frame = CGRect(x: 0, y: self.frame.height - thickness, width: self.frame.width, height: thickness)
-            bottomBorder.backgroundColor = color.cgColor
-            self.layer.addSublayer(bottomBorder)
-        }
+        let bottomBorder = CALayer()
+        bottomBorder.frame = CGRect(x: 0, y: self.frame.height - thickness, width: self.frame.width, height: thickness)
+        bottomBorder.backgroundColor = color.cgColor
+        self.layer.addSublayer(bottomBorder)
+    }
+    
+    func setGradientBackground() {
+        layer.shadowColor = UIColor.black.withAlphaComponent(0.25).cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 1.0
+        layer.masksToBounds = false
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [#colorLiteral(red: 0.6106664538, green: 0.6106664538, blue: 0.6106664538, alpha: 1).withAlphaComponent(0.54).cgColor, UIColor.white.cgColor]
+        gradientLayer.cornerRadius = 8
+//        gradientLayer.locations = [0.1476, 1.0]
+        gradientLayer.locations = [0.0, 0.7, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        layer.insertSublayer(gradientLayer, at: 0)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
--- Car_Dealer/Car_Dealer/Menu/MenuViewController.swift
+++ Car_Dealer/Car_Dealer/Menu/MenuViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet var sectionButtons: [UIButton]!
+    @IBOutlet var sectionsView: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sectionsView.forEach({ $0.setGradientBackground() })
+        sectionButtons.forEach({ $0.titleLabel?.font = .semibold(size: 22) })
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
     @IBAction func clickedMyCars(_ sender: UIButton) {
@@ -18,5 +31,11 @@ class MenuViewController: UIViewController {
         self.navigationController?.pushViewController(myCarsVC, animated: true)
     }
     
+    @IBAction func clickedAddCar(_ sender: UIButton) {
+    }
+    @IBAction func clickedReports(_ sender: UIButton) {
+    }
+    @IBAction func clickedHelp(_ sender: UIButton) {
+    }
 }
 
